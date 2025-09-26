3d32fdc45c8007604df22275ec427206bbdb05df
pavlovaesp <gekat8@gmail.com>
2025-09-26 20:38:39 +0300

    Playwright2

--- playwright_autorization.py
+++ playwright_autorization.py
@@ -8,19 +8,22 @@ with sync_playwright() as playwright:
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
     ##//div[@data-testid="login-form-email-input"]//div/input - локатор со страницы
-    email_input = page.locator('//div[@data-testid="login-form-email-input"]//div/input')
+    # правильно использовать упрощенные локаторы
+    # используем функцию get_by_test_id и эту часть локатора значение > "login-form-email-input"
+    # в случае с почтой/паролем мы указываем imput так как надо искать внутри контейнера
+    email_input = page.get_by_test_id('login-form-email-input').locator('input')
     email_input.fill('user.name@gmail@mail.ru')
 
     ##//div[@data-testid="login-form-password-input"]//div/input
-    password_input = page.locator('//div[@data-testid="login-form-password-input"]//div/input')
+    password_input = page.get_by_test_id('login-form-password-input').locator('input')
     password_input.fill('Password')
 
     ##//button[@data-testid="login-page-login-button"]
-    login_button = page.locator('//button[@data-testid="login-page-login-button"]')
+    login_button = page.get_by_test_id('login-page-login-button')
     login_button.click()
 
     ##//div[@data-testid="login-page-wrong-email-or-password-alert"]
-    wrong_email_or_password_alert = page.locator('//div[@data-testid="login-page-wrong-email-or-password-alert"]')
+    wrong_email_or_password_alert = page.get_by_test_id('login-page-wrong-email-or-password-alert')
     expect(wrong_email_or_password_alert).to_be_visible() ##visible виден/ hiden невидем
     expect(wrong_email_or_password_alert).to_have_text('Wrong email or password')
 
