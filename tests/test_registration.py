7228f409234310777a88e2d99a8db6ad3417a98a
pavlovaesp <gekat8@gmail.com>
2025-10-01 22:25:01 +0300

    pytest2

---
+++ tests/test_registration.py
@@ -0,0 +1,38 @@
+from  playwright.sync_api import sync_playwright
+
+
+# чтоб обернуть файл playwright в тест нам надо
+def test_successeful_registration():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)  ## False когда хотим видеть открытый браузер
+        context = browser.new_context()  # используется для сохранения
+
+        page = context.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+        email_input.fill('username@gmail@mail.ru')
+
+        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+        username_input.fill('username')
+
+        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+        password_input.fill('Password')
+
+        ##//button[@data-testid="login-page-login-button"]
+        registration_button = page.get_by_test_id('registration-page-registration-button')
+        registration_button.click()
+
+        # теперь сохраняем данные сверху
+        context.storage_state(path='browser-state.json')
+
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(headless=False)
+        context = browser.new_context(storage_state='browser-state.json')
+        page = context.new_page()
+
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+
+        page.wait_for_timeout(4000)
+
