eeca748527feec7009b08a6bc4fa2bb7ee9ac6a7
pavlovaesp <gekat8@gmail.com>
2025-10-08 23:47:19 +0300

    homework_fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,10 +12,53 @@ from playwright.sync_api import sync_playwright, Page, Playwright
 #         browser.close()
 
 # yield позволяет выполнить действия до и после теста
+# При первом вызове выполнение начинается с начала функции до первого yield. После этого функция «замораживает» своё состояние.
+# При следующем вызове выполнение продолжается сразу после yield. Каждое значение, которое возвращает yield, становится элементом итерации.
+# Когда инструкции заканчиваются, генератор выбрасывает исключение StopIteration — программа понимает, что элементы закончились, и останавливает работу.
 
+import pytest
+from playwright.sync_api import Playwright, Page
 
 @pytest.fixture
-def chromium_page(playwright) -> Page:   # возвращает тип Page
-        browser = playwright.chromium.launch(headless=False)  ## False когда хотим видеть открытый браузер
-        yield browser.new_page()
-        browser.close()
+def chromium_page(playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
+
+
+@pytest.fixture(scope="session")
+def initialize_browser_state(playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('username@gmail@mail.ru')
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('Password')
+
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_button.click()
+
+    page.wait_for_timeout(3000)
+
+    context.storage_state(path='browser-state.json')
+    yield page
+    context.close()
+    browser.close()
+
+
+@pytest.fixture(scope="function")
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+    yield page     # ✅ теперь возвращаем страницу
+    context.close()
+    browser.close()
--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,64 +1,25 @@
-from playwright.sync_api import sync_playwright, expect
+import pytest
+from playwright.sync_api import sync_playwright, expect, Page
 
-def test_empty_courses_list():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)  ## False когда хотим видеть открытый браузер
-        context = browser.new_context()  # используется для сохранения
-
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-
-        email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-        email_input.fill('username@gmail@mail.ru')
-
-        username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-        username_input.fill('username')
-
-        password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-        password_input.fill('Password')
-
-        registration_button = page.get_by_test_id('registration-page-registration-button')
-        registration_button.click()
-
-        page.wait_for_timeout(3000)
-
-        # теперь сохраняем данные сверху
-        context.storage_state(path='browser-state.json')
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='browser-state.json')
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
-
-        # теперь сохраняем данные сверху
-        context.storage_state(path='browser-state.json')
-
-        page.wait_for_timeout(3000)
-
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=False)
-        context = browser.new_context(storage_state='browser-state.json')
-        page = context.new_page()
-
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+@pytest.mark.courses
+@pytest.mark.regression
+def test_empty_courses_list(chromium_page_with_state, chromium_page: Page):
+        chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
         # data-testid="courses-list-toolbar-title-text"
-        courses_list_toolbar = page.get_by_test_id('courses-list-toolbar-title-text')
+        courses_list_toolbar = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
         expect(courses_list_toolbar).to_be_visible()
 
         # data-testid="courses-list-empty-view-icon"
-        view_icon = page.get_by_test_id('courses-list-empty-view-icon')
+        view_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
         expect(view_icon).to_be_visible()
 
         # data-testid="courses-list-empty-view-title-text"
-        title_text = page.get_by_test_id('courses-list-empty-view-title-text')
+        title_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
         expect(title_text).to_be_visible()
 
         # data-testid="courses-list-empty-view-description-text"
-        description_text = page.get_by_test_id('courses-list-empty-view-description-text')
+        description_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
         expect(description_text).to_be_visible()
 
-        page.wait_for_timeout(3000)
+        chromium_page_with_state.wait_for_timeout(3000)
