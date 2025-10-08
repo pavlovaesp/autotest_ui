87f5df8589833b9bcfe137d7b96c28ade590f0b5
pavlovaesp <gekat8@gmail.com>
2025-10-08 22:27:05 +0300

    pytest-playwright

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,13 +2,20 @@
 # название файла только такое
 
 import pytest
-from playwright.sync_api import sync_playwright, Page
+from playwright.sync_api import sync_playwright, Page, Playwright
+
+# @pytest.fixture
+# def chromium_page() -> Page:   # возвращает тип Page
+#     with sync_playwright() as playwright:
+#         browser = playwright.chromium.launch(headless=False)  ## False когда хотим видеть открытый браузер
+#         yield browser.new_page()
+#         browser.close()
+
+# yield позволяет выполнить действия до и после теста
+
 
 @pytest.fixture
-def chromium_page() -> Page:   # возвращает тип Page
-    with sync_playwright() as playwright:
+def chromium_page(playwright) -> Page:   # возвращает тип Page
         browser = playwright.chromium.launch(headless=False)  ## False когда хотим видеть открытый браузер
         yield browser.new_page()
         browser.close()
-
-# yield позволяет выполнить действия до и после теста
