eb503176115697f88bcdf4abeb3cfe229badd661
pavlovaesp <gekat8@gmail.com>
2025-10-21 22:36:05 +0300

    7.2.PageObject

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -1,6 +1,7 @@
 import pytest
 from playwright.sync_api import Page
 
+from pages.courses_list_page import CoursesListPage
 from pages.login_page import LoginPage
 from pages.registration_page import RegistrationPage
 from pages.dashboard_page import DashboardPage
@@ -18,3 +19,7 @@ def registration_page(initialize_browser_state: Page) -> RegistrationPage:
 @pytest.fixture(scope="function")
 def dashboard_page(chromium_page_with_state: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page_with_state)
+
+@pytest.fixture
+def courses_list_page(chromium_page: Page) -> CoursesListPage:
+    return CoursesListPage(page=chromium_page)
