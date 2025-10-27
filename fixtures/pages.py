3f1ab5c83f80ed1b7bf1efe95f37b6ca82cc7ca5
pavlovaesp <gekat8@gmail.com>
2025-10-27 20:09:38 +0300

    PageObject3

--- fixtures/pages.py
+++ fixtures/pages.py
@@ -2,6 +2,7 @@ import pytest
 from playwright.sync_api import Page
 
 from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 from pages.login_page import LoginPage
 from pages.registration_page import RegistrationPage
 from pages.dashboard_page import DashboardPage
@@ -19,5 +20,9 @@ def dashboard_page(chromium_page: Page) -> DashboardPage:
     return DashboardPage(page=chromium_page)
 
 @pytest.fixture
-def courses_list_page(chromium_page: Page) -> CoursesListPage:
+def courses_list_page(chromium_page_with_state: Page) -> CoursesListPage:
     return CoursesListPage(page=chromium_page)
+
+@pytest.fixture
+def create_courses_page(chromium_page_with_state: Page) -> CreateCoursePage:
+    return CreateCoursePage(page=chromium_page)
